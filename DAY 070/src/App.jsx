7608203f3f550d5132b885e7f5229a028aa2e186
Meet Hirpara <meetkumarhirpara@gmail.com>
2025-06-01 23:10:08 +0530

    Daily backup - $(1-June-2025)

---
+++ DAY 070/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>App | Hello Word</div>
+  )
+}
+
+export default App;
+export const x =  'HelloX';
+export const y =  'HelloY';
---
+++ DAY 070/src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx';
+import {x, y} from './App.jsx';
+console.log(x, y);
+
+
+createRoot(document.getElementById('root')).render(<App />);
