ae8ca13d813895fc1ef0b7e3f6154ee5393bd7a1
Meet Hirpara <meetkumarhirpara@gmail.com>
2025-06-16 19:41:37 +0530

    Daily backup - $(16-June-2025)

--- DAY 081/ROUTING DAY-81/src/App.jsx
+++ DAY 081/ROUTING DAY-81/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='w-screen h-screen bg-gray-600'>App</div>
   )
 }
 
-export default App
+export default App;
