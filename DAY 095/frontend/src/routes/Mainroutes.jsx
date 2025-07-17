ed9397cedc787d0d159b2acdb2b7a9ec2c684fec
Meet Hirpara <meetkumarhirpara@gmail.com>
2025-07-17 14:04:34 +0530

    Daily backup - $(17-July-2025)

---
+++ DAY 095/frontend/src/routes/Mainroutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home } from "../pages/Home";
+import { Product } from "../pages/Product";
+import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
+
+const Mainroutes = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Home />} /> 
+        <Route path='/products' element={<Product />} /> 
+        <Route path='/login' element={<Login />} /> 
+        <Route path='/register' element={<Register />} /> 
+    </Routes>
+  )
+}
+
+export default Mainroutes;
