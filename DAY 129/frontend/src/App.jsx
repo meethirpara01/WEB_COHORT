e4243a4f0426a48a5338062da9ac08cd6fc6778d
Meet Hirpara <meetkumarhirpara@gmail.com>
2025-08-05 21:20:22 +0530

    Daily backup - $(5-Aug-2025)

---
+++ DAY 129/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import FaceExpressionDetector from './components/FaceExpressionDetector'
+import MoodSongs from './components/MoodSong';
+
+function App() {
+
+  return (
+    <>
+      <FaceExpressionDetector />
+      <MoodSongs />
+    </>
+  )
+}
+
+export default App;
---
+++ DAY 129/frontend/src/components/MoodSong.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import "../components/MoodSongs.css"
+
+const MoodSongs = () => {
+    const [Songs, setSongs] =   useState([
+        {
+            title: "test_title",
+            artise: "test_artist",
+            url: "test_url"
+        },
+        {
+            title: "test_title",
+            artise: "test_artist",
+            url: "test_url"
+        },
+        {
+            title: "test_title",
+            artise: "test_artist",
+            url: "test_url"
+        },
+    ])
+  return (
+    <div className='mood-song'>
+        <h2>Recommended Songs</h2>
+        {Songs.map((song, index) => (
+            <div key={index} className='song'>
+                <div className='title'>
+                    <h3>{song.title}</h3>
+                    <h3>{song.artise}</h3>
+                </div>
+                <div className="play-pause-button">
+                    <i class="ri-pause-large-line"></i>
+                    <i class="ri-play-circle-fill"></i>
+                </div>
+                
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default MoodSongs
