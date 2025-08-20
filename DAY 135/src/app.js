b37787f424dd66fc81c31a70821a40073fbfeeac
Meet Hirpara <meetkumarhirpara@gmail.com>
2025-08-20 16:41:36 +0530

    Daily backup - $(20-Aug-2025)

---
+++ DAY 135/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const authRoutes = require("./routes/auth.routes");
+
+const app = express();
+app.use(express.json());
+
+
+app.use('/auth', authRoutes);
+// app.use('/product', productRoutes);
+// app.use('/customer', customerRoutes);
+
+module.exports = app;
