5ee93beee1f3904769afeb54041f9f7e4ebfb32c
Meet Hirpara <meetkumarhirpara@gmail.com>
2025-08-24 22:08:07 +0530

    Daily backup - $(24-Aug-2025)

--- DAY 135/PART-2/src/app.js
+++ DAY 135/PART-2/src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const v = require('./routes/auth.routes');
+const authRoutes = require('./routes/auth.routes');
+const cookieParser = require('cookie-parser');
 
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.use('/auth', authRoutes);
+
 
 
 module.exports = app;
--- DAY 135/PART-2/src/routes/auth.routes.js
+++ DAY 135/PART-2/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userModel = require('../model/user.model');
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -13,7 +14,7 @@ router.post('/register', async (req, res) =>
 
     if (isuser)
     {
-        return res.status(401).json({
+        return res.status(409).json({
             message: "USERNAME IS ALREDY TAKEN!"
         });
     }
@@ -22,5 +23,94 @@ router.post('/register', async (req, res) =>
         username, password
     });
 
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.SECRATE_KEY);
+
+    res.cookie('token', token);
+
+    res.status(201).json({
+        message: "USER REGISTERED SUCCESSFULLY",
+        user
+    });
+});
+
+router.get('/user', async (req, res) =>
+{
+    const token = req.cookies.token;
+
+    if(!token)
+    {
+        return res.status(401).json({
+            message:"UNATHORIZED TOKEN NOT FOUND!"
+        })
+    }
+
     
+    try
+    {
+        const decoded = jwt.verify(token, process.env.SECRATE_KEY);
+
+        const user = await userModel.findOne({
+            _id: decoded.id
+        });
+
+        res.status(201).json({
+            message: "USER DATA FATCH SUCCESSFULLY",
+            user
+        });
+
+    }catch(err)
+    {
+        res.status(401).json({
+            message: "UNATHORIZED TOKEN NOT FOUND!"
+        });
+
+    }
+});
+
+
+router.post('/login', async (req, res) =>
+{
+    const {username, password} = req.body;
+
+    const user = await userModel.findOne({username});
+
+    if(!user)
+    {
+        return  res.status(404).json({
+            message: "USER ACCOUNT NOT FOUND!"
+        });
+    }
+
+    const ispassword = await userModel.findOne({password});
+
+    if(!ispassword)
+    {
+        return res.status(404).json({
+            message: "INVALID PASSWORD!"
+        });
+    }
+
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.SECRATE_KEY);
+
+    res.cookie("token", token);
+
+    res.status(201).json({
+        message: "USER LOGIN SUCCESSFULLY",
+        user
+    });
+});
+
+router.get('/logout', (req, res) =>
+{
+    res.clearCookie("token");
+
+    res.status(200).json({
+        message: "USER LOGGED OUT SUCCESSFULLY"
+    })
 })
+
+module.exports = router;
