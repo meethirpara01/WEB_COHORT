4f6f71b8ca3b507c0c7ec6e51037fe6e564be5c2
Meet. Hirpara <meetkumarhirpara@gmail.com>
2025-09-01 19:31:38 +0530

    Daily backup - $(1-Sep-2025)

--- DAY 139/src/routes/post.routes.js
+++ DAY 139/src/routes/post.routes.js
@@ -1,4 +1,35 @@
 const express = require("express");
+const jwt = require('jsonwebtoken');
+const userModel = require('../model/user.model');
 const router = express.Router();
 
+
+router.post('/', async (req, res) =>
+{
+    const token = req.cookies.token;
+
+    if(!token)
+    {
+        return res.status(401).json({
+            message: "UNAUTHORIZRED TOKEN, PLEASE LOGIN FIRST!"
+        });
+    }
+
+    try
+    {
+        const decoded = jwt.verify(token, process.env.SECRATE_KEY);
+
+        const user = await userModel.findOne({
+            _id: decoded.id
+        });
+
+        req.user = user;
+    }
+    catch (error)
+    {
+        return res.status(401).json({
+            message: "INVALID TOKEN, PLEASE LOGIN AGAIN!"
+        });
+    }
+});
 module.exports = router;
